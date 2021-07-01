f0c3addfc5b16cde9c8201094ec9241b37836107
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2021-07-01 20:01:04 +0200

    chore: added primefaces

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,16 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import PrimeVue from 'primevue/config';
+import 'primeflex/primeflex.css';
+import 'primevue/resources/themes/saga-blue/theme.css';
+import 'primevue/resources/primevue.min.css';
+import 'primeicons/primeicons.css';
 
 import VueCompositionAPI from '@vue/composition-api';
 
 Vue.use(VueCompositionAPI);
+Vue.use(PrimeVue);
 
 Vue.config.productionTip = false;
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,13 @@
-<template><div></div></template>
+<template>
+  <div class="p-d-flex"></div>
+</template>
 
 <script lang="ts">
 import Vue from 'vue';
+import Button from 'primevue/button';
+import Card from 'primevue/card';
 
 export default Vue.extend({
   name: 'Home',
-
-  components: {},
 });
 </script>
